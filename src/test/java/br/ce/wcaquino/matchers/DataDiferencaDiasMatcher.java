6aea4957af7f8205c3a972ae91c3c56a220bc5b3
Dean Araujo <deantarik4@gmail.com>
2024-04-10 19:32:38 -0300

    Matchers challenge

---
+++ src/test/java/br/ce/wcaquino/matchers/DataDiferencaDiasMatcher.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.matchers;
+
+import java.util.Date;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+import br.ce.wcaquino.utils.DataUtils;
+
+public class DataDiferencaDiasMatcher extends TypeSafeMatcher<Date> {
+
+	private Integer qtdDias;
+	
+	public DataDiferencaDiasMatcher(Integer qtdDias) {
+		this.qtdDias = qtdDias;
+	}
+	
+	@Override
+	public void describeTo(Description desc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean matchesSafely(Date data) {
+		return DataUtils.isMesmaData(data, DataUtils.obterDataComDiferencaDias(qtdDias));
+	}
+
+}
