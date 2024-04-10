84de44c2b6b2828b57d1db8a33c19d890a7bf9a7
Dean Araujo <deantarik4@gmail.com>
2024-04-10 00:48:34 -0300

    TDD (pt 1)

---
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -0,0 +1,22 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class Calculadora {
+
+	public int somar(int a, int b) {
+		return a + b;
+	}
+
+	public int subtrair(Integer a, Integer b) {
+		return a - b;
+	}
+
+	public double dividir(double x, double y) throws NaoPodeDividirPorZeroException {
+		if (y == 0) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+		return x / y;
+	}
+
+}
---
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,65 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class CalculadoraTest {
+
+	private Calculadora calc;
+	private int a;
+	private int b;
+	
+	@Before
+	public void setup() {
+		calc = new Calculadora();
+		a = 5;
+		b = 3;
+	}
+	
+	
+	@Test
+	public void deveSomarDoisValores() {
+		//cenario
+		
+		//acao
+		int resultado = calc.somar(a, b);
+		
+		//verificacao
+		Assert.assertEquals(8, resultado);
+	}
+	
+	@Test
+	public void deveSubtrairDoisValores() {
+		//cenario
+		
+		//acao
+		int resultado = calc.subtrair(a, b);
+		
+		//verificacao
+		Assert.assertEquals(2, resultado);
+	}
+	
+	@Test
+	public void deveDividirDoisValores() throws NaoPodeDividirPorZeroException {
+		//cenario
+		double x = 5.0;
+		double y = 2.0;
+		
+		//acao
+		double resultado = calc.dividir(x, y);
+		
+		//verificacao
+		Assert.assertEquals(resultado, resultado, 0.01);
+	}
+	
+	@Test(expected = NaoPodeDividirPorZeroException.class)
+	public void deveLancarExcecaoAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		int x = 10;
+		int y = 0;
+		
+		calc.dividir(x, y);
+	}
+}
